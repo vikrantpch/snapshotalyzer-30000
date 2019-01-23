e73fc8afeafc4d026ddd3cd744336df93051cfbb
vikrantpch <vikrant.p.ch@gmail.com>
2019-01-22 20:00:58 -0500

    Added project name to instance data

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,17 +4,32 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2=session.resource('ec2')
 
+@click.group()
+def instances():
+    """Commands for instances"""
 
-@click.command()
-def list_instances():
+@instances.command()
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
     "List EC2 Instances"
-    for i in ec2.instances.all():
+    instances = []
+    tags={}
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        tags={t['Key']: t['Value'] for t in i.tags or []}
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name)))
+            i.public_dns_name,
+            tags.get('Project','<no project>'))))
 
     return
 
